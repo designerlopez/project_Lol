3e3c90b0c6e85a0871a83554e048ebf28225d306
edgar_lopez <edgarlopezacevedo@gmail.com>
2022-11-24 17:16:08 -0500

    estilos a la card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import './App.css'
+import './index.css'
 import Card from './components/Card'
 
 import Navbar from './components/Navbar'
@@ -8,36 +8,60 @@ import Navbar from './components/Navbar'
 function App() {
 
   const [champions,setChampion] = useState([])
+  
+  const [counterSkins,setCounterSkins] = useState([])
+
 
   useEffect(()=>{
     const url="http://ddragon.leagueoflegends.com/cdn/12.22.1/data/es_ES/champion.json"
     axios.get(url)
       .then(res=>{
         const {data}=res.data
+        console.log(data)
         const keysChampions=Object.keys(data)
         const championsFormat=keysChampions.map(champion=>({
           name:champion,
-          img: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion}_0.jpg`,
+          img: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion}_1.jpg`,
           stats:data[champion].stats,
+          info:data[champion].info,
           
         }))
-        
-        
       setChampion(championsFormat)   
       })
       
 
   }, []) 
 
+
+  const changeSkin=()=>{
+    setCounterSkins(counterSkins+1)
+    console.log(counterSkins)
+  }
+
+
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
       <h1>League of legends Database</h1>
 
-      <h2>{champions[3]?.name}</h2> 
-      <img src={champions[3]?.img} alt="champion" />
-      <button>clic aca para cambiar</button>
+      <Card
+      name={champions[1]?.name} 
+      img={champions[1]?.img}
+      hp={champions[1]?.stats.hp}
+      attack={champions[1]?.stats.hp}
+      damage={champions[1]?.stats.hp}
+      movespeed={champions[1]?.stats.hp}
+      />
+        <h2>{champions[1]?.name}</h2> 
+      <img src={champions[1]?.img} alt="champion" />
+      <button onClick={changeSkin}>clic aca para cambiar campeon</button>
+      <h3> Health: {champions[1]?.stats.hp}</h3>
+      <h3> Armadura: {champions[1]?.stats.armor}</h3>
+      <h3> Damage: {champions[1]?.stats.attackdamage}</h3>
+      <h3> Move Speed: {champions[1]?.stats.movespeed}</h3>    
       
+      
+    
 
       
       
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-export const Card = ({champion}) => {
-
+export const Card = ({name, img, attack, hp, damage, movespeed}) => {
 
     return (
       
-      <div className="card">
-        <h3>{champion}</h3>
-        <ul>
-          <li>{champion}</li>
-          <li> </li>
-          <li> </li>
-          <li> </li>
-        </ul>
+      <div className="card animate-bg">
+        <h2>{name}</h2>
+        {/* <h4>Title</h4>*/}
+
+        <div className='card_img'>
+        <img src={img} alt="" />
+        </div>
+        <div className='card_stats'>
+        
+          <h3 className='card_stats-info'>Attack: {attack} </h3>
+          <h3 className='card_stats-info'>Hp: {hp}</h3>
+          <h3 className='card_stats-info'>Damage: {damage}</h3>
+          <h3 className='card_stats-info'>Move Speed: {movespeed} </h3>
+        
+        </div>
       </div>
     );
   };
